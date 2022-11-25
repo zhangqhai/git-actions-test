c2a97ea467575f3971b3c5113f9ada8e64c94c75
QingHai ZHANG <zqh1017@foxmail.com>
2022-11-25 10:06:11 +0800

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
     public static void main(String[] args) {
         // test1
-        System.out.println(System.getenv());
+        System.out.println(System.getenv())
     }
 }
