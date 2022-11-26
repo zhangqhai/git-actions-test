a51e6e1487929552e8b0e774ce943bac1860d319
guanjingchen <32256771+guanjingchen@users.noreply.github.com>
2022-11-26 16:21:04 +0800

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo {
     public static void main(String[] args) {
         // test1234
         System.out.println(System.getenv());
-        System.out.println(“cgj test ci/cd”;
+        System.out.println("cgj test action");
     }
 }
